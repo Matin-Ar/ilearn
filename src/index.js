c0a26ae2586d9b1087a776c52161e1478760b00d
Matin-Crazy <araghimatin@gmail.com>
2021-09-09 17:57:41 +0430

    Deploying to heroku

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const activationRouter = require('./routers/activation')
@@ -7,10 +8,14 @@ const courseRouter = require('./routers/course')
 const app = express()
 const port = process.env.PORT
 
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 app.use(express.json())
 app.use('/api', userRouter)
 app.use('/api', activationRouter)
 app.use('/api', courseRouter)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+})
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
