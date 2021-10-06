2822ca497352772e787701a6788aa42f9c0e6750
Matin-Crazy <araghimatin@gmail.com>
2021-10-06 19:49:25 +0330

    ready for heroku

--- server/models/course.js
+++ server/models/course.js
@@ -5,7 +5,8 @@ const courseSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        unique: true
     },
     price: {
         type: String,
